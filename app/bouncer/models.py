f8a8a0631f222f8e0b882879050f51e5281161a3
Julio <julio@ctrlsun.com>
2021-08-18 21:47:32 -0500

    Added a model datastore property.

--- app/bouncer/models.py
+++ app/bouncer/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+"""Datastore models."""
+from google.cloud import ndb
 
-# Create your models here.
+
+class Redirect(ndb.Model):
+    name = ndb.StringProperty()
+    destination_url = ndb.StringProperty()
+    created_on = ndb.DateTimeProperty(auto_now_add=True)
