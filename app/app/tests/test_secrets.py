573155c885d7b70d26c1a8159f60a00d025de77c
Julio <julio@ctrlsun.com>
2021-08-27 11:06:37 -0500

    Before starting section 5: Build Bouncer.

--- app/app/tests/test_secrets.py
+++ app/app/tests/test_secrets.py
@@ -20,4 +20,37 @@ class SecretManagerTests(SimpleTestCase):
 
     def test_retrieve_secret(self, mock_sm):
         """Test getting a secret from secret manager."""
-        
+        mock_client = Mock()
+        mock_sm.return_value = mock_client
+        mock_version_res = Mock()
+        secret = "samplesecret123"
+        secret_bytes = bytes(secret, "utf-8")
+        mock_version_res.payload.data = secret_bytes
+        mock_client.access_secret_version.return_value = mock_version_res
+
+        name = "SampleSecretName"
+        ret = secrets.get(name)
+
+        exp_path = (
+            f"projects/{MOCK_GCP_PROJECT}/secrets/"
+            f"{name}/versions/latest"
+        )
+        mock_client.access_secret_version.assert_called_once_with(
+            request={"name": exp_path}
+        )
+        self.assertEqual(ret, secret)
+
+
+class LocalDevModeTests(SimpleTestCase):
+    """Tests for getting config from env vars."""
+
+    @patch("app.secrets.SecretManagerServiceClient")
+    def test_retrieve_from_env_var(self, mock_sm):
+        """Test getting value from env var."""
+        secret = "SampleSecret123"
+        name = "secretname"
+        with patch.dict("os.environ", {name: secret}):
+            val = secrets.get(name)
+
+        self.assertEqual(val, secret)
+        mock_sm.assert_not_called()
